1994575154373466172f44d24a62549c6afcf1c7
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-01-22 17:55:48 +0900

    세션 고정 보호

--- src/main/java/com/example/springstudysecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springstudysecurity/config/SecurityConfig.java
@@ -19,17 +19,31 @@ public class SecurityConfig {
     //권한필터 메소드
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((auth)-> auth
-                .requestMatchers("/","/login","/loginProc","/join","/joinProc").permitAll()
-                .requestMatchers("/admin").hasRole("ADMIN")
-                .requestMatchers("/my/**").hasAnyRole("ADMIN","USER")
-                .anyRequest().authenticated()
+        http
+                .authorizeHttpRequests((auth)-> auth
+                        .requestMatchers("/","/login","/loginProc","/join","/joinProc").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN","USER")
+                        .anyRequest().authenticated()
         );
 
-        http.formLogin((auth)->auth.loginPage("/login")
-                .loginProcessingUrl("/loginProc")
-                .permitAll()
+        http
+                .formLogin((auth)->auth.loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll()
         );
+
+        http
+                .sessionManagement((auth)->auth
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true));
+
+        http
+                .sessionManagement((auth) -> auth
+                        .sessionFixation((sessionFixation) -> sessionFixation
+                                .changeSessionId()
+                        )
+                );
 
         http.csrf((auth)->auth.disable());
         return http.build();
