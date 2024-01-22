7704df7b661d4b6314516f4f927b58e5dc173436
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-01-22 15:24:51 +0900

    Update SecurityConfig.java
    비밀번호 암호화 메소드 생성

--- src/main/java/com/example/springstudysecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springstudysecurity/config/SecurityConfig.java
@@ -4,11 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    //비밀번호 암호화 메소드
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //권한필터 메소드
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests((auth)-> auth
