3250c2e199162c68d7dd810bffae2f1df1784046
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-01-22 14:06:23 +0900

    init

---
+++ src/main/java/com/example/springstudysecurity/SpringStudySecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springstudysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudySecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudySecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springstudysecurity/SpringStudySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstudysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudySecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
