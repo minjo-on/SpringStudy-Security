78cf946ba6f8c11e86cc102d8ad5ce977d2e27f0
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-01-23 18:43:03 +0900

    Login Filter 구현

--- src/main/java/com/example/springstudysecurity/jwtConfig/JwtSecurityConfig.java
+++ src/main/java/com/example/springstudysecurity/jwtConfig/JwtSecurityConfig.java
@@ -1,20 +1,35 @@
 package com.example.springstudysecurity.jwtConfig;
 
+import com.example.springstudysecurity.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class JwtSecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public JwtSecurityConfig(AuthenticationConfiguration authenticationConfiguration){
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
 
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -37,6 +52,9 @@ public class JwtSecurityConfig {
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
