38d333a4d63936982db0863a08be4577c5b6d91e
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-01-24 15:18:15 +0900

    토큰 필터 생성

---
+++ src/main/java/com/example/springstudysecurity/jwt/JWTFilter.java
@@ -0,0 +1,59 @@
+package com.example.springstudysecurity.jwt;
+
+import com.example.springstudysecurity.dto.CustomUserDetails;
+import com.example.springstudysecurity.entity.UserEntity;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JWTFilter extends OncePerRequestFilter {
+    private final JWTUtil jwtUtil;
+    public JWTFilter(JWTUtil jwtUtil){
+        this.jwtUtil = jwtUtil;
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizations = request.getHeader("Authorization");
+
+        //header 토큰 검증
+        if(authorizations == null || !authorizations.startsWith("Bearer ")){
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        System.out.println("authorization now");
+        String token = authorizations.split(" ")[1];
+
+        //token 유효시간 검증
+        if(jwtUtil.isExpired(token)){
+            System.out.println("token expired");
+            filterChain.doFilter(request,response);
+
+            return;
+        }
+
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(username);
+        userEntity.setPassword("temppassword");
+        userEntity.setRole(role);
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request,response);
+    }
+}
