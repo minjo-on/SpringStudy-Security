b8137a35a2acfdfa06a37c5cbcbad52b4339a3ac
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-02-27 10:05:19 +0900

    권한 인가 처리

--- src/main/java/com/example/springstudysecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springstudysecurity/config/SecurityConfig.java
@@ -4,30 +4,36 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
     UserDetailsService userDetailsService;
 
     //비밀번호 암호화 메소드
@@ -40,11 +46,37 @@ public class SecurityConfig {
 
         return roleHierarchy;
     }
+    @Bean
+    public UserDetailsService users(){
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+
+        UserDetails sys = User.builder()
+                .username("sys")
+                .password("{noop}1111")
+                .roles("SYS")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1111")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,sys,admin);
+    }
     //권한필터 메소드
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .authorizeHttpRequests((auth)-> auth
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/user").hasRole("USER")
+                        .requestMatchers("/admin/pay").hasRole("ADMIN")
+                        .requestMatchers("/admin/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') or hasRole('SYS')"))
                         .anyRequest().authenticated()
 
                 );
@@ -60,8 +92,10 @@ public class SecurityConfig {
                         .successHandler(new AuthenticationSuccessHandler() {
                             @Override
                             public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                                System.out.println("authentication" + authentication.getName());
-                                response.sendRedirect("/");
+                                RequestCache requestCache = new HttpSessionRequestCache();
+                                SavedRequest savedRequest = requestCache.getRequest(request,response);
+                                String redirectUrl = savedRequest.getRedirectUrl();
+                                response.sendRedirect(redirectUrl);
                             }
                         })
                         .failureHandler(new AuthenticationFailureHandler() {
@@ -106,6 +140,22 @@ public class SecurityConfig {
                         .sessionFixation().changeSessionId()
                         .maximumSessions(1)
                         .maxSessionsPreventsLogin(true)
+                );
+
+        http
+                .exceptionHandling((exception) -> exception
+                        /*.authenticationEntryPoint(new AuthenticationEntryPoint() {
+                            @Override
+                            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                                response.sendRedirect("/login");
+                            }
+                        })*/
+                        .accessDeniedHandler(new AccessDeniedHandler() {
+                            @Override
+                            public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                                response.sendRedirect("/denied");
+                            }
+                        })
                 );
 
         return http.build();
--- src/main/java/com/example/springstudysecurity/controller/AdminController.java
+++ src/main/java/com/example/springstudysecurity/controller/AdminController.java
@@ -1,12 +1,33 @@
 package com.example.springstudysecurity.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AdminController {
-    @GetMapping("/admin")
-    public String adminP(){
+    @GetMapping("/admin/pay")
+    public String adminPay()
+    {
+        return "adminPay";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/admin/**")
+    public String admin(){
         return "admin";
+    }
+
+    @GetMapping("/denied")
+    public String denied(){
+        return "denied";
     }
 }
